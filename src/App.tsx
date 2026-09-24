2fc5d3971c019964f8aa5d7971fd12d49c336190
Rwema01 <r.gisa@alustudent.com>
2026-09-24 21:38:23 +0200

    feat: render TaskManager in App and polish theme styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import Navbar from "./components/Navbar";
-import { ThemeProvider } from "./context/ThemeContext";
+import TaskManager from "./components/TaskManager";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
+import { LIGHT_THEME } from "./constants/theme";
 import "./App.css";
+
+function AppContent() {
+  const { theme } = useTheme();
+
+  return (
+    <div className={theme === LIGHT_THEME ? "app light" : "app dark"}>
+      <Navbar />
+      <TaskManager />
+    </div>
+  );
+}
 
 function App() {
   return (
     <ThemeProvider>
-      <Navbar />
+      <AppContent />
     </ThemeProvider>
   );
 }
